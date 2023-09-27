3377044be95ffc2b62039f0b24529d658f1c2612
Alexandr Cocier <Limos12345@outlook.com>
2023-09-27 06:54:01 +0300

    Create entities

---
+++ OnlineShop.Api/Entities/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShop.Api.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ OnlineShop.Api/Entities/ProductCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShop.Api.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
