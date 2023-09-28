7afcf7299e820d2b88c81c9c41e29f35084d9c0c
Alexandr Cocier <Limos12345@outlook.com>
2023-09-28 07:28:59 +0300

    Create the Database using EF Core Code (Database Migrations)

--- OnlineShop.Api/Program.cs
+++ OnlineShop.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection")));
 
 var app = builder.Build();
 
