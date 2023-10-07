11a2d25023c5643be14384863d11fee03c419c3d
Alexandr Cocier <Limos12345@outlook.com>
2023-10-07 15:49:23 +0300

    Update the Quantity of Products in the Shopping Cart (Web API, Blazor, Blazor JavaScript Interoperability)

--- ShopOnline.Web/Services/ShoppingCartService.cs
+++ ShopOnline.Web/Services/ShoppingCartService.cs
@@ -1,6 +1,9 @@
-﻿using ShopOnline.Models.Dtos;
+﻿using Newtonsoft.Json;
+using ShopOnline.Models.Dtos;
 using ShopOnline.Web.Services.Contracts;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace ShopOnline.Web.Services
 {
@@ -82,6 +85,29 @@ namespace ShopOnline.Web.Services
             }
             catch (Exception)
             {
+                throw;
+            }
+        }
+
+        public async Task<CartItemDto> UpdateQty(CartItemQtyUpdateDto cartItemQtyUpdateDto)
+        {
+            try
+            {
+                var jsonRequest = JsonConvert.SerializeObject(cartItemQtyUpdateDto);
+                var content = new StringContent(jsonRequest,Encoding.UTF8,"application/json-patch+json");
+
+                var response = await httpClient.PatchAsync($"api/ShoppingCart/{cartItemQtyUpdateDto.CartItemId}", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<CartItemDto>();
+                }
+
+                return null;
+            }
+            catch (Exception)
+            {
+
                 throw;
             }
         }
